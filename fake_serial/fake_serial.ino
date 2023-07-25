81f487db03efcdd4d505385027ff29d16fc777b3
Brett(Jiaxin) Yang <brett.yang@hotmail.com>
2023-07-25 19:59:44 -0400

    allow Brett's Arduino to produce fake outputs
    remember to change serial port to COM4

---
+++ fake_serial/fake_serial.ino
@@ -0,0 +1,37 @@
+int delayTime = 1000;
+int distanceCm = 15;
+int mode = 1;
+
+int i = 0;
+int dummy = 100;
+
+
+void setup() {
+  // Initialize serial communication
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  // sensor geting data...
+  delayMicroseconds(2);
+  delayMicroseconds(10);
+
+  // software delay
+  i += 1;
+  if (i == dummy) {
+    i = 0;
+    distanceCm += 10;
+    if (distanceCm >= 30) distanceCm = 15;
+  }
+
+
+  // Send {delay, distance} to the Serial Monitor
+  Serial.print(delayTime);
+  Serial.print("ms, ");
+  Serial.print(distanceCm);
+  Serial.print("cm, mode=");
+  Serial.println(mode);
+
+  delay(10);
+}
